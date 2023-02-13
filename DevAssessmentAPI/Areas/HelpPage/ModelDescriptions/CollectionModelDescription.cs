22418c24939ab53c550bfbd5b9324e21416b58c2
Gautam Manorkar <gautam.r.manorkar@gmail.com>
2023-02-13 16:35:23 +0100

    Deleted Unwanted Files
    Deleted Unwanted Files

--- DevAssessmentAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace DevAssessmentAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- DevAssessmentAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace DevAssessmentAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
